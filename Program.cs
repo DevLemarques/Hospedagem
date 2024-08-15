44a2c1966a3b3ab6d72566599a7ab114abf0a04a
DevLemarques <dev.lemarques@gmail.com>
2024-08-15 13:58:46 -0300

    Reapply "Sistema de estacionamento"
    This reverts commit 79bd5b6035149d6555fa7c1d3f730e6de3efd56a.

--- Program.cs
+++ Program.cs
@@ -6,19 +6,25 @@ Console.OutputEncoding = Encoding.UTF8;
 // Cria os modelos de hóspedes e cadastra na lista de hóspedes
 List<Pessoa> hospedes = new List<Pessoa>();
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+Pessoa p1 = new Pessoa(nome: "Leandro", sobrenome: "Marques");
+Pessoa p2 = new Pessoa(nome: "Tawani", sobrenome: "Marques");
+Pessoa p3 = new Pessoa(nome: "Tarcisio", sobrenome: "Marques");
 
 hospedes.Add(p1);
 hospedes.Add(p2);
+hospedes.Add(p3);  
+
+
 
 // Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 5, valorDiaria: 30);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
+Reserva reserva = new Reserva(diasReservados: 12);
+
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
+
 
 // Exibe a quantidade de hóspedes e o valor da diária
 Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
